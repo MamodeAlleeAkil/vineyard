da6507f4be45d756effe75ea5acc62b8882b96a2
Tofy Zubair <tofy.zubair@gmail.com>
2018-05-08 15:19:08 +0400

    Get data from rows

--- Akil/script.js
+++ Akil/script.js
@@ -117,3 +117,23 @@ else if(nic.length < 14 || nic.length > 14) {
 
 
 //}
+
+
+$(document).ready(function () {
+
+  // code to read selected table row cell data (values).
+  console.warn("first");
+  $(".products-table").on('click', '.add-button', function () {
+    console.warn("second");
+    // get the current row
+    var currentRow = $(this).closest("tr");
+
+    var col1 = currentRow.find("td:eq(0)").text(); // get current row 1st TD value
+    var col2 = currentRow.find("td:eq(1)").text(); // get current row 2nd TD
+    //var col3 = currentRow.find("td:eq(2)").text(); // get current row 3rd TD
+    var quantity = currentRow.find(".quantity").val()
+    var data = col1 + "\n" + col2 + "\n" + quantity;
+
+    alert(data);
+  });
+});
